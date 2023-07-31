226ee7cf14b8db42e390a6eb7ef86d022ccb8b98
LevoratoJoao <j.vitor.jvlds@gmail.com>
2023-07-30 22:12:20 -0300

    Create New Page is done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -28,32 +28,52 @@ def entry(request, pk):
 def search(request):
     if request.method == "GET":
         searched = request.GET.get('q')
-        if searched == "":
-            # Empty search
-            entries = list(filter(lambda x: searched in x, util.list_entries()))
-            return render(request, "encyclopedia/search.html", {
-            "search": "",
-            "entries": None
-        })
         entry = util.get_entry(searched)
-        if entry == None:
-            # Show all the results with that search
-            entries = list(filter(lambda x: searched in x, util.list_entries()))
+        if searched != "":
+            if entry != None:
+                # Redirect the user to the entry page
+                htmlData = Markdown().convert(entry)
+                return render(request, "encyclopedia/entry.html", {
+                    "data": htmlData,
+                    "titlePage": searched
+                })
+            else:
+                # Show all the results with that search
+                entries = list(filter(lambda x: searched in x, util.list_entries()))
+                return render(request, "encyclopedia/search.html", {
+                    "search": searched,
+                    "entries": entries
+                })
+        else:
+            # Empty search
             return render(request, "encyclopedia/search.html", {
-            "search": searched,
-            "entries": entries
-        })
-        # Redirect the user to the entry page
-        htmlData = Markdown().convert(entry)
-        return render(request, "encyclopedia/entry.html", {
-        "data": htmlData,
-        "titlePage": searched
-    })
+                "search": searched,
+                "entries": None
+            })
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def create(request):
+    if request.method == "POST":
+        form = NewPageText(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            text = form.cleaned_data.get('text')
+            searchEqual = list(filter(lambda x: title in x, util.list_entries()))
+            if searchEqual == []:
+                util.save_entry(title, text)
+                return entry(request, title)
+            else:
+                return render(request, "encyclopedia/create.html", {
+                "title": "Error",
+                "form": form
+            })
+        else:
+            return render(request, "encyclopedia/create.html", {
+                "title": "Create New Page",
+                "form": form
+            })
     return render(request, "encyclopedia/create.html", {
         "title": "Create New Page",
         "form": NewPageText
